b80d4bc39594a6ecb9ecd73a7bc6f8c4522673e8
Anurag Peshne <anurag.peshne@gmail.com>
2020-04-25 23:13:50 -0700

    adds parsing logic for headings

--- src/org_parser/parser.clj
+++ src/org_parser/parser.clj
@@ -1,10 +1,5 @@
 (ns org-parser.parser)
 
-(defn- parse-heading
-  [heading-type content parsed-ast WIP-node]
-                                        ; TODO add as a child node or attach the WIP node and create a new node)
-  )
-
 (defrecord ASTNode [type val children])
 
 (defn- get-heading-terminating-condition
@@ -20,18 +15,23 @@
   ([tokens]
    (let [root-terminating-condition #(true)
          [children-node _] (parse tokens root-terminating-condition)
-         root-node (ASTNode. (:type :root) (:val nil) (:children children-node))]
+         root-node (ASTNode. :root nil children-node)]
      root-node))
   ([tokens terminating-condition]
    (loop [acc-children []
-          unprocessed-token tokens]
-     (when (seq unprocessed-tokens)
+          unprocessed-tokens tokens]
+     (if (seq unprocessed-tokens)
        (let [[current-token & rest-tokens] tokens
              [token-type & token-content] current-token
              [returned-node returned-unprocessed-tokens]
              (case token-type
-               :head (let [current-heading-level (second current-token)]
-                       (parse
-                        rest-tokens
-                        (get-heading-terminating-condition current-heading-level))))]
-         (recur (conj acc-children returned-node) returned-unprocessed-tokens))))))
+               :head (let [current-heading-level (second current-token)
+                           [children-node returned-unprocessed-tokens]
+                           (parse
+                            rest-tokens
+                            (get-heading-terminating-condition current-heading-level))]
+                       [(ASTNode. :head token-content children-node)
+                        returned-unprocessed-tokens])
+               (throw (str "unknown token: " token-type)))]
+         (recur (conj acc-children returned-node) returned-unprocessed-tokens))
+       [acc-children unprocessed-tokens]))))
--- test/org_parser/parser_test.clj
+++ test/org_parser/parser_test.clj
@@ -1,14 +1,19 @@
 (ns org-parser.parser-test
-  (:require [org-parser.parser :as parser]
-            [org-parser.tokenizer :as tok]
-            [clojure.test :as t]))
+  (:require [clojure.test :refer :all]
+            [org-parser.parser :as parser]
+            [org-parser.tokenizer :as tok])
+  (:import [org_parser.parser ASTNode]))
 
 (deftest heading-test
   (testing "Simple h1"
     (let [input "* Head"
-          exp-out {:type :head
-                   :val [1 [:text Head]]
-                   :children []}
+          exp-out (parser/ASTNode.
+                   :root
+                   nil
+                   [(parser/ASTNode.
+                     :head
+                     [1 [[:text "Head"]]]
+                     [])])
           actual-out (-> input
                          tok/tokenize
                          parser/parse)]
